a2a68cc871813a14d53ee8b28fe24300d31b53ef
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-11-23 22:28:22 +0530

    implement authentication and authorization features with JWT

--- aurora-be/src/index.ts
+++ aurora-be/src/index.ts
@@ -3,36 +3,41 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
 const app = express();
 
-// CORS middleware configuration
+// CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
-)
+);
 
-// JSON parsing middleware
+// Middlewares
 app.use(express.json());
-
-// Cookie parsing middleware
 app.use(cookieParser());
+
+// Routes
+app.use('/api/v1/auth', authRoutes)
 
 // Test route
 app.get("/", (req, res) => {
   res.json({ message: "Backend running..." });
 });
 
-// Start
+// Start server
 const startServer = async () => {
   await connectDB();
-  app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`));
+  app.listen(SERVER_PORT, () =>
+    console.log(`Server running on port ${SERVER_PORT}`)
+  );
 };
 
 startServer();
